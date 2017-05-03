48b5dc17f126c888103d8b64373c8dcd1982c19b
zengchao <zengchao_cool@126.com>
2017-05-03 17:49:39 +0800

    add c style log

--- test.cpp
+++ test.cpp
@@ -34,6 +34,14 @@ int main(int argc, char* argv[])
     LOGGER(error) << "An log file error severity message";
     LOGGER(fatal) << "A log file fatal severity message";
 
+    logger::set_log_level(debug);
+    LOG(info, "start c style log");
+    LOG(debug, "c style: A debug severity message %d, %s, %ld", 1, "test", 2);
+    LOG(info, "c style: An informational severity %s", "message");
+    LOG(warn, "c style: A warning severity %s", "message");
+    LOG(error, "c style: An error %s message", "severity");
+    LOG(fatal, "c style: A %s severity message", "fatal");
+
     int i;
     std::cin >> i;
 
