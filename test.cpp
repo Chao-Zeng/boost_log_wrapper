325e59409176d6cb4c42c6347a4a7b1c68b70c37
Chao-Zeng <zengchao_cool@126.com>
2019-03-26 14:02:30 +0800

    add macro

--- test.cpp
+++ test.cpp
@@ -35,12 +35,12 @@ int main(int argc, char* argv[])
     LOGGER(fatal) << "A log file fatal severity message";
 
     logger::set_log_level(debug);
-    LOG(info, "start c style log");
-    LOG(debug, "c style: A debug severity message %d, %s, %ld", 1, "test", 2);
-    LOG(info, "c style: An informational severity %s", "message");
-    LOG(warn, "c style: A warning severity %s", "message");
-    LOG(error, "c style: An error %s message", "severity");
-    LOG(fatal, "c style: A %s severity message", "fatal");
+    LOG_INFO("start c style log");
+    LOG_DEBUG("c style: A debug severity message %d, %s, %ld", 1, "test", 2);
+    LOG_INFO("c style: An informational severity %s", "message");
+    LOG_WARN("c style: A warning severity %s", "message");
+    LOG_ERROR("c style: An error %s message", "severity");
+    LOG_FATAL("c style: A %s severity message", "fatal");
 
     int i;
     std::cin >> i;
